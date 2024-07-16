ddfdb6f3467a3bdc15e1f1d0a9170371f2c3735e
Stringy Wingy <aaronmatthew16@gmail.com>
2024-07-16 03:57:05 -0400

    replaced the previous tweakable quad with a subdivision quad that holds up to shearing and stretching much, much better

--- projection_quad_2d.gd
+++ projection_quad_2d.gd
@@ -1,85 +1,127 @@
 @tool
 extends MeshInstance2D
 
-@export var start_size : Vector2
+@export var start_size : Vector2 = Vector2(200,200)
+@export var subdivision_resolution : Vector2 = Vector2(4,4)
+
+var xverts = subdivision_resolution.x + 1
+var yverts = subdivision_resolution.y + 1
 
 var mesh_data = []
+var handles = []
 
-func build_mesh() -> void:
-	pass
+var vertices = PackedVector3Array() 
+var uvs = PackedVector2Array() 
+var indices = PackedInt32Array() 
+
+var needs_rebuild := false
+
 
 func v3_v2(vec: Vector3) -> Vector2:
 	return Vector2(vec.x,vec.y)
 
-func calc_vert5() -> Vector3:
-	var p1 = mesh_data[ArrayMesh.ARRAY_VERTEX][0]
-	var p2 = mesh_data[ArrayMesh.ARRAY_VERTEX][3]
-	var q1 = mesh_data[ArrayMesh.ARRAY_VERTEX][1]
-	var q2 = mesh_data[ArrayMesh.ARRAY_VERTEX][2]
-	return Geometry3D.get_closest_points_between_segments(p1,p2,q1,q2)[0] #the point should be the intersection if all z coords are 0
+func v2_v3(vec: Vector2) -> Vector3:
+	return Vector3(vec.x,vec.y,0)
 
+func init_mesh() -> void:
+	uvs.clear()
+	vertices.clear()
+	indices.clear()
 
-func calc_uv5():
-	var p1 = mesh_data[ArrayMesh.ARRAY_TEX_UV][0]
-	var p2 = mesh_data[ArrayMesh.ARRAY_TEX_UV][3]
-	var q1 = mesh_data[ArrayMesh.ARRAY_TEX_UV][1]
-	var q2 = mesh_data[ArrayMesh.ARRAY_TEX_UV][2]
-	return Geometry2D.segment_intersects_segment(p1, p2, q1, q2); #this may return null if the segments don't intersect
+	xverts = subdivision_resolution.x + 1
+	yverts = subdivision_resolution.y + 1
+
+	#create grid of vertices
+	for j in yverts:
+		for i in xverts:
+			#helpful to have normalized cords progressing along the mesh's limits
+			var frac = Vector2(i,j) / subdivision_resolution
+
+			#init uvs as full 0-1
+			var uv = Vector2(frac.x,frac.y)
+
+			var pos2 = (start_size * frac) - (start_size / 2)
+			var pos = v2_v3(pos2)
+
+			uvs.append(uv)
+			vertices.append(pos)
+	#end creating vertices
+
+	#order up triangles
+	#strategy: go one quad at a time
+	#  0  1
+	#  2  3
+	#  for each subdiv quad, crate triangles [0, 1, 2] and [1,3,2]
+
+	for j in subdivision_resolution.y:
+		for i in subdivision_resolution.x:
+			var v0 = j * xverts + i
+			var v1 = v0 + 1
+			var v2 = (j + 1) * xverts + i
+			var v3 = v2 + 1
+
+			var quad = PackedInt32Array([
+				v0, v1, v2, #top tri
+				v1, v3, v2 #bot tri
+				])
+
+			indices.append_array(quad)
+	#end ordering triangles
+
+	#slap all this data into the surface
+	mesh_data[ArrayMesh.ARRAY_VERTEX] = vertices
+	mesh_data[ArrayMesh.ARRAY_TEX_UV] = uvs
+	mesh_data[ArrayMesh.ARRAY_INDEX] =  indices
+	mesh.clear_surfaces()
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
 	
+
+func rebuild_positions() -> void:
+
+	vertices.clear()
+
+	for j in yverts:
+		for i in xverts:
+			#get positions along the 'top' and 'bottom' edges (in handle space) based on the x index coordinate of this vertex
+			var p1 = handles[0].lerp(handles[1], i/subdivision_resolution.x)
+			var p2 = handles[2].lerp(handles[3], i/subdivision_resolution.x)
+
+			#lerp between those two positions to create a y axis in handle space
+			var p = p1.lerp(p2, j/subdivision_resolution.y)
+			vertices.append(v2_v3(p))
+
+	mesh_data[ArrayMesh.ARRAY_VERTEX] = vertices
+	mesh_data[ArrayMesh.ARRAY_TEX_UV] = uvs
+	mesh_data[ArrayMesh.ARRAY_INDEX] =  indices
+	mesh.clear_surfaces()
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	mesh_data.resize(ArrayMesh.ARRAY_MAX)
-
-	#we create a quad out of 5 vertices, 4 triangles, allowing us to cleanly sheer/tilt/whatever our 2d texture
-	mesh_data[ArrayMesh.ARRAY_VERTEX] = PackedVector3Array(
-			[
-				Vector3(-start_size.x/2, -start_size.y/2, 0), #top left
-				Vector3(start_size.x/2, -start_size.y/2, 0), #top right
-				Vector3(-start_size.x/2, start_size.y/2, 0), #bot left
-				Vector3(start_size.x/2, start_size.y/2, 0), #bot right
-				Vector3(0, 0, 0), #center
-			]
-		)
-
-	mesh_data[ArrayMesh.ARRAY_TEX_UV] = PackedVector2Array(
-			[
-				Vector2(0,0), #top left
-				Vector2(1,0), #top right
-				Vector2(0,1), #bot left
-				Vector2(1,1), #bot right
-				Vector2(0.5,0.5), #center
-			]
-		)
-
-	mesh_data[ArrayMesh.ARRAY_INDEX] = PackedInt32Array(
-			[
-				4, 0, 1, #top
-				4, 1, 3, #right
-				4, 3, 2, #bot
-				4, 2, 0, #left
-			]
-		)
-
-	mesh = ArrayMesh.new()
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
-
 	#update handle positions in editor
 	$vert_handle_0.position = Vector2(-start_size.x/2, -start_size.y/2) - $vert_handle_0.size/2 #top left
 	$vert_handle_1.position = Vector2(start_size.x/2, -start_size.y/2) - $vert_handle_1.size/2 #top right
 	$vert_handle_2.position = Vector2(-start_size.x/2, start_size.y/2) - $vert_handle_2.size/2 #bot left
 	$vert_handle_3.position = Vector2(start_size.x/2, start_size.y/2) - $vert_handle_3.size/2 #bot right
 
+	handles = [
+		$vert_handle_0.position + $vert_handle_0.size/2,
+		$vert_handle_1.position + $vert_handle_1.size/2,
+        $vert_handle_2.position + $vert_handle_2.size/2,
+        $vert_handle_3.position + $vert_handle_3.size/2
+	]
+
+	mesh = ArrayMesh.new()
+	mesh_data.resize(ArrayMesh.ARRAY_MAX)
+	init_mesh()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if needs_rebuild:
+		rebuild_positions()
+		needs_rebuild = false
 
 func on_vertex_handle_moved(index, local_position):
-	mesh_data[ArrayMesh.ARRAY_VERTEX].set(index, Vector3(local_position.x, local_position.y, 0))
-	
-	#upate the 5th vertex
-	var v5 = calc_vert5()
-	mesh_data[ArrayMesh.ARRAY_VERTEX].set(4, v5)
-
-	mesh.clear_surfaces()
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
+	handles[index] = local_position
+	needs_rebuild = true
